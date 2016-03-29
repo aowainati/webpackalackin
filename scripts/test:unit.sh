6855da2d07e525db98570925dfe92a4d1bc85339
Ahmed Owainati <aowainati2099@gmail.com>
2016-03-29 15:12:45 -0700

    Please the indentation gods

--- scripts/test:unit.sh
+++ scripts/test:unit.sh
@@ -5,17 +5,17 @@ FILES=($(find test/unit -name "*.js"))
 
 OPTIONS=()
 if [[ "${CIRCLE_TEST_REPORTS}" != "" ]]; then
-    OPTIONS+=(
-        --reporter mocha-circleci-reporter
-        --reporter-options mochaFile="${CIRCLE_TEST_REPORTS}"/test:unit.xml
-    )
+  OPTIONS+=(
+      --reporter mocha-circleci-reporter
+      --reporter-options mochaFile="${CIRCLE_TEST_REPORTS}"/test:unit.xml
+  )
 fi
 
 # TODO : Set NODE_ENV to anything other than development so react-transform-hmr doesn't kick in (from .babelrc)
 #        We should probably make 'test' a VARIANT and fetch it that way.
 NODE_ENV=test ./node_modules/.bin/mocha \
-    "${OPTIONS[@]}" \
-    --require ./test/env/boot \
-    --require ./test/env/unit \
-    "$@" \
-    "${FILES[@]}"
+  "${OPTIONS[@]}" \
+  --require ./test/env/boot \
+  --require ./test/env/unit \
+  "$@" \
+  "${FILES[@]}"
