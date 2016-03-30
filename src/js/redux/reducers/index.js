3c3895b94758c368e889727d1c6bf9a741b7fff6
Ahmed Owainati <aowainati2099@gmail.com>
2016-03-30 14:21:39 -0700

    Remove old reducer file

--- src/js/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import combinedReduction from 'combined-reduction';
-
-import * as interactions from '../interactions';
-
-export default combinedReduction({
-  theButton: interactions.theButton.reducer,
-});
